4a6cb2b5e55fe1cbcd67dbd5b53d8009b41bc551
Aeron <ajlaffere@gmail.com>
2024-06-19 23:59:15 +0100

    gradient working with sound meter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import SoundMeter from './components/SoundMeter';
+import "./App.css";
+import SoundMeter from "./components/SoundMeter.js";
+import { MeshGradientRenderer } from "@johnn-e/react-mesh-gradient";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SoundMeter />
-</header>
+    <div>
+      <MeshGradientRenderer
+        id="gradient-container"
+        className="gradient"
+        colors={["#C3E4FF", "#6EC3F4", "#EAE2FF", "#B9BEFF", "#B3B8F9"]}
+        wireframe={true}
+      />
+      <SoundMeter />
     </div>
   );
 }
+
+// The general idea is that this should look quite calm but when you start recording it bursts into colour, emanating out from the button
 
 export default App;
